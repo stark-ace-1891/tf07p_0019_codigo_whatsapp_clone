44a8309e894c4c5c6db0882983a9a3a9b894645d
Cesar Morales <cesar.aml1891@gmail.com>
2023-05-14 03:10:33 -0500

    Refactorizando aplicando condicionales en items

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,31 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:tf07p_0019_codigo_whatsapp_clone/data/data_dummy.dart';
 import 'package:tf07p_0019_codigo_whatsapp_clone/widget/item_chat_widget.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-          ItemChatWidget(),
-        ],
-      ),
+    return ListView.builder(
+      itemCount: chats.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ItemChatWidget(
+          // avatarUrl: chats[index].avatarUrl,
+          // username: chats[index].username,
+          chatModel: chats[index],
+        );
+      },
     );
   }
 }
--- lib/widget/item_chat_widget.dart
+++ lib/widget/item_chat_widget.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:tf07p_0019_codigo_whatsapp_clone/models/chat_model.dart';
 
 class ItemChatWidget extends StatelessWidget {
-  const ItemChatWidget({
-    super.key,
+  // String avatarUrl;
+  // String username;
+  ChatModel chatModel;
+
+  ItemChatWidget({
+    // required this.avatarUrl,
+    // required this.username,
+    required this.chatModel,
   });
 
   @override
@@ -15,47 +22,53 @@ class ItemChatWidget extends StatelessWidget {
         leading: CircleAvatar(
           backgroundColor: Colors.black12,
           radius: 26,
-          backgroundImage: NetworkImage(
-              "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+          backgroundImage: NetworkImage(chatModel.avatarUrl),
         ),
         title: Text(
-          "Fiorella Guadalupe Lopez",
+          chatModel.username,
           style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
         ),
         subtitle: Text(
-          "He enviado el encargo por curier por favor tienes que estar atento",
+          chatModel.isTyping == true ? "Typing" : chatModel.message,
           maxLines: 1,
           overflow: TextOverflow.ellipsis,
           style: TextStyle(
             fontSize: 13,
+            color:
+                chatModel.isTyping == true ? Color(0xff01c851) : Colors.black45,
           ),
         ),
         trailing: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "10:20",
+              chatModel.time,
               style: TextStyle(
                 fontSize: 12,
-                color: Color(0xff01c851),
+                color: chatModel.countMessage > 0
+                    ? Color(0xff01c851)
+                    : Colors.black45,
               ),
             ),
-            Container(
-              width: 20,
-              height: 20,
-              decoration: BoxDecoration(
-                color: Color(0xff01c851),
-                shape: BoxShape.circle,
-              ),
-              alignment: Alignment.center,
-              child: Text(
-                "1",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
+            if (chatModel.countMessage > 0)
+              Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: Color(0xff01c851),
+                  shape: BoxShape.circle,
                 ),
-              ),
-            ),
+                alignment: Alignment.center,
+                child: Text(
+                  chatModel.countMessage.toString(),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                  ),
+                ),
+              )
+            else
+              SizedBox(),
           ],
         ),
       ),
