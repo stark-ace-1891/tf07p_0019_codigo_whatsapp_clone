b1a4f13c44ec329ef507c5d7db6eeede54ffe6e3
Cesar Morales <cesar.aml1891@gmail.com>
2023-05-14 02:23:52 -0500

    Usando UserChatModel

---
+++ lib/data/data_dummy.dart
@@ -0,0 +1,58 @@
+import 'package:tf07p_0019_codigo_whatsapp_clone/models/chat_model.dart';
+
+List<ChatModel> chats = [
+  ChatModel(
+    avatarUrl:
+        "https://images.pexels.com/photos/1845534/pexels-photo-1845534.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    username: "Ximena Lopez",
+    message: "Ya he enviando el encargo por el courier",
+    time: "10:20",
+    countMessage: 1,
+    isTyping: false,
+  ),
+  ChatModel(
+    avatarUrl:
+        "https://images.pexels.com/photos/4922971/pexels-photo-4922971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    username: "Juan Carlos Lopez",
+    message: "Acabo de llegar a la reunión, por favor envia eso",
+    time: "12:40",
+    countMessage: 3,
+    isTyping: true,
+  ),
+  ChatModel(
+    avatarUrl:
+        "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    username: "Karla Gonzales",
+    message: "Flutter es genial!",
+    time: "15:22",
+    countMessage: 0,
+    isTyping: false,
+  ),
+  ChatModel(
+    avatarUrl:
+        "https://images.pexels.com/photos/8422403/pexels-photo-8422403.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    username: "Daniel Montes Arias",
+    message: "Hola, cómo estas?",
+    time: "11:22",
+    countMessage: 1,
+    isTyping: false,
+  ),
+  ChatModel(
+    avatarUrl:
+        "https://images.pexels.com/photos/1845534/pexels-photo-1845534.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    username: "Ximena Lopez",
+    message: "Ya he enviando el encargo por el courier",
+    time: "10:20",
+    countMessage: 1,
+    isTyping: false,
+  ),
+  ChatModel(
+    avatarUrl:
+        "https://images.pexels.com/photos/4922971/pexels-photo-4922971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    username: "Juan Carlos Lopez",
+    message: "Acabo de llegar a la reunión, por favor envia eso",
+    time: "12:40",
+    countMessage: 3,
+    isTyping: true,
+  ),
+];
