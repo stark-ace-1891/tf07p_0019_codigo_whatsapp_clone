61efa05b417b435d1473b7133f4294aef75e9b49
Cesar Morales <cesar.aml1891@gmail.com>
2023-05-14 00:22:24 -0500

    Proyecto inicial

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomPage extends StatelessWidget {
+  const HomPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Prueba"),
+      ),
+    );
+  }
+}
