9f26de96ea3b5e7b763e2faf6dab4d07648c49e3
Cesar Morales <cesar.aml1891@gmail.com>
2023-05-14 00:40:08 -0500

    Definiendo temas en el MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,13 @@ class MyApp extends StatelessWidget {
       title: "Whatsapp Clone",
       debugShowCheckedModeBanner: false,
       home: HomPage(),
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            color: Color(0xff065e52),
+          ),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Color(0xff01c851),
+          )),
     );
   }
 }
